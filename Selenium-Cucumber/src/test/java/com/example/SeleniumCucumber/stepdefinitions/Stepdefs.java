d751b0979b05e6a5aa485a4e43425f180aef1e3b
Nicklas Carlsson <nicklas.carlsson@gmail.com>
2023-04-06 22:30:46 +0200

    Add final testcases. Add sync.

--- Selenium-Cucumber/src/test/java/com/example/SeleniumCucumber/stepdefinitions/Stepdefs.java
+++ Selenium-Cucumber/src/test/java/com/example/SeleniumCucumber/stepdefinitions/Stepdefs.java
@@ -13,7 +13,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -28,6 +31,7 @@ public class Stepdefs {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("incognito");
+        //options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -52,76 +56,69 @@ public class Stepdefs {
     }
 
 
-    //@Given("SVT play is available")
-    public void svtPlayIsAvailable() {
-        // First load of page is done in beforeAll setUp and also handles the cookie popup. This method loads the initial page so we know where the test starts.
-        driver.get("https://svtplay.se");
-
-    }
-
-    @Given("User visits SVT Play")
+    @Given("user visits SVT Play")
     public void userVisitsSVTPlay() {
         driver.get("https://svtplay.se");//Before each normally navigates to page, but doing it here as well for better test understandability.
     }
 
-    @Then("The browser title should be {string}")
+    @Then("the browser title should be {string}")
     public void theBrowserTitleShouldBe(String expectedTitle) {
         assertEquals(expectedTitle, driver.getTitle(), "Page title is not as expected");
     }
 
     public static void sleep(int numberOfSeconds) {
         try {
-            Thread.sleep(numberOfSeconds * 1000); // Sleep for number of seconds seconds
+            Thread.sleep(numberOfSeconds * 1000); // Sleep for number of seconds
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    @Then("Logo should be visible")
+    @Then("logo should be visible")
     public void logoShouldBeVisible() {
         WebElement logo = driver.findElement(By.xpath("//a[@data-rt=\"play-logo\" and @href=\"/\"]"));
         assertEquals(true, logo.isDisplayed(), "I could not find the logo..");
     }
 
-    @Then("Start link should exist and have the text {string}")
+    @Then("start link should exist and have the text {string}")
     public void startLinkShouldExistAndHaveTheText(String expectedText) {
         WebElement startLink = driver.findElement(By.xpath("//a[@href=\"/\" and @accesskey=1]"));
         assertEquals(expectedText, startLink.getText(), "Start link did not have correct text");
     }
 
-    @Then("Program link should exist and have the text {string}")
+    @Then("program link should exist and have the text {string}")
     public void programLinkShouldExistAndHaveTheText(String expectedText) {
         WebElement startLink = driver.findElement(By.xpath("//a[@href=\"/program\" and @accesskey=2]"));
         assertEquals(expectedText, startLink.getText(), "PROGRAM link did not have correct text");
 
     }
 
-    @Then("Kanaler link should exist and have the text {string}")
+    @Then("kanaler link should exist and have the text {string}")
     public void kanalerLinkShouldExistAndHaveTheText(String expectedText) {
         WebElement kanalertLink = driver.findElement(By.xpath("//a[@href=\"/kanaler\" and @accesskey=3]"));
         assertEquals(expectedText, kanalertLink.getText(), "KANALER link did not have correct text");
 
     }
 
-    @Then("Tillganglighet link should exist")
+    @Then("tillganglighet link should exist")
     public void tillgänglighetLinkShouldExist() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/tillganglighet']"));
         assertEquals(true, link.isDisplayed(), "The link 'Tillgänglighet i SVT Play' does not display...");
     }
 
-    @And("User clicks tillganglighet link")
+    @And("user clicks tillganglighet link")
     public void userClicksTillganglighetLink() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/tillganglighet']"));
         link.click();
     }
 
-    @Then("Tillgänglighet page should display with header text {string}")
+    @Then("tillgänglighet page should display with header text {string}")
     public void tillgänglighetPageShouldDisplayWithHeaderText(String expectedHeaderText) {
         WebElement header = driver.findElement(By.xpath("//h1"));
         assertEquals(expectedHeaderText, header.getText(), "Headertexten på tillgänglighetssidan stämmer ej");
     }
 
-    @And("User clicks on Program link")
+    @And("user clicks on Program link")
     public void userClicksOnProgramLink() {
         WebElement startLink = driver.findElement(By.xpath("//a[@href=\"/program\" and @accesskey=2]"));
         startLink.click();
@@ -137,7 +134,7 @@ public class Stepdefs {
 
     }
 
-    @And("User search for the text {string}")
+    @And("user search for the text {string}")
     public void userSearchForTheText(String searchForText) {
         WebElement searchField = driver.findElement(By.cssSelector("input[name='q']"));
         WebElement searchButton = driver.findElement(By.xpath("//button[@type='submit' and @title='Sök på svtplay.se']"));
@@ -147,7 +144,7 @@ public class Stepdefs {
 
     }
 
-    @Then("search results header for text {string} should display.")
+    @Then("search results header for text {string} should display")
     public void searchResultsHeaderShouldDisplay(String searchedForText) {
         // This method only verifies header, not the exact amount of hits.
         WebElement searchResult = driver.findElement(By.xpath("//h2[@data-rt='header-search-result']"));
@@ -159,7 +156,7 @@ public class Stepdefs {
 
     }
 
-    @Then("Cookie information link should display")
+    @Then("cookie information link should display")
     public void cookieInformationLinkShouldDisplay() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/cookies-och-personuppgifter']"));
         String text = link.getText();
@@ -168,32 +165,32 @@ public class Stepdefs {
         assertEquals("Om cookies och personuppgifter", textSelection, "cookie information link text is not correct");
     }
 
-    @And("User clicks cookie information link")
+    @And("user clicks cookie information link")
     public void userClicksCookieInformationLink() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/cookies-och-personuppgifter']"));
         link.click();
     }
 
-    @Then("Cookie information page should display")
+    @Then("cookie information page should display")
     public void cookieInformationPageShouldDisplay() {
         WebElement header = driver.findElement(By.className("text-3xl"));
         assertEquals("Om cookies och personuppgiftsbehandling på SVT", header.getText(), "cookie information header text is not correct");
     }
 
-    @And("User clicks kanaler link")
+    @And("user clicks kanaler link")
     public void userClicksKanalerLink() {
         WebElement kanalerLink = driver.findElement(By.xpath("//a[@href=\"/kanaler\" and @accesskey=3]"));
         kanalerLink.click();
     }
 
-    @Then("TVTablå for {string} should display.")
+    @Then("TVTablå for {string} should display")
     public void tvtablåForShouldDisplay(String TVChannel) {
         WebElement section = driver.findElement(By.cssSelector("section[aria-label='Tablå för " + TVChannel + "']"));
         assertEquals(true, section.isDisplayed(), "Tablå for " + TVChannel + " did not display");
 
     }
 
-    @Then("Nyhetsbrev link should display")
+    @Then("nyhetsbrev link should display")
     public void nyhetsbrevLinkShouldDisplay() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://nyhetsbrev.svtplay.se/prenumerera/?utm_source=svtplay&utm_medium=footer-cta']"));
         String text = link.getText();
@@ -201,14 +198,14 @@ public class Stepdefs {
         assertEquals("Nyhetsbrev", textSelection, "Nyhetsbrev link text is not correct");
     }
 
-    @And("User clicks Nyhetsbrev link")
+    @And("user clicks Nyhetsbrev link")
     public void userClicksNyhetsbrevLink() {
         WebElement link = driver.findElement(By.xpath("//a[@href='https://nyhetsbrev.svtplay.se/prenumerera/?utm_source=svtplay&utm_medium=footer-cta']"));
         link.click();
 
     }
 
-    @Then("Nyhetsbrev page should display")
+    @Then("nyhetsbrev page should display")
     public void nyhetsbrevPageShouldDisplay() {
         WebElement header = driver.findElement(By.xpath("//h1"));
         assertEquals("Missa inga program och serier!", header.getText(), "nyhetsbrev title page not correct");
@@ -251,6 +248,11 @@ public class Stepdefs {
 
     @Then("{string} number of episodes are displaying")
     public void episodesAreDisplaying(String expectedNumberOfEpisodes) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Waiting for the first h3 element to appear (To complete page loading)
+        WebElement firstH3Element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h3")));
+
+        // Now getting all H3 elements
         List<WebElement> season2EpisodesList = driver.findElements(By.tagName("h3"));
 
         // Going through all H3 elements. We are only interested in elements that contains text. I did not know how to filter that using findElements, so I did it programatically instead.
@@ -265,6 +267,10 @@ public class Stepdefs {
 
     @Then("episode {string} is called {string}")
     public void episodeIsCalled(String episodeNumber, String expectedTitle) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        // Waiting for the first h3 element to appear (To complete page loading)
+        WebElement firstH3Element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h3")));
+
         List<WebElement> season2EpisodesList = driver.findElements(By.tagName("h3"));
         int numberOfEpisodes = 0;
         for (WebElement episode : season2EpisodesList) {
@@ -273,5 +279,33 @@ public class Stepdefs {
             }
         }
         assertEquals(expectedTitle, season2EpisodesList.get(Integer.parseInt(episodeNumber)-1).getText(), "episode " + episodeNumber + " name should be " + expectedTitle + ", but it is not");
+    }
+
+    @Then("dator link should display")
+    public void datorLinkShouldDisplay() {
+        WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/svt-play-webb']"));
+        String text = link.getText();
+        String textSelection = text.substring(0, 5);
+
+        assertEquals("Dator", textSelection, "Dator information link text starts with Dator, OK!");
+    }
+
+
+    @When("user clicks on dator link")
+    public void userClicksOnDatorLink() {
+        WebElement link = driver.findElement(By.xpath("//a[@href='https://kontakt.svt.se/guide/svt-play-webb']"));
+        link.click();
+    }
+
+    @Then("computer requirements page displays")
+    public void computerRequirementsPageDisplays() {
+        WebElement header = driver.findElement(By.cssSelector("H1"));
+        assertEquals("Systemkrav för SVT Play på webben", header.getText(), "Page did not display with correct text");
+    }
+
+    @When("user clicks Karen Pirie: Kalla fall")
+    public void userClicksKarenPirieKallaFall() {
+        WebElement link = driver.findElement(By.xpath("//em[normalize-space()='Karen Pirie: Kalla fall']"));
+        link.click();
     }
 }
--- Selenium-Cucumber/src/test/resources/features/SVTPlay-Selenium-tests.feature
+++ Selenium-Cucumber/src/test/resources/features/SVTPlay-Selenium-tests.feature
@@ -1,93 +1,92 @@
 Feature: Test page navigations and correct browser titles
 
   Scenario: SVT Play should show correct title
-    Given User visits SVT Play
-    Then The browser title should be "SVT Play"
-
+    Given user visits SVT Play
+    Then the browser title should be "SVT Play"
 
   Scenario: SVT Play logo should be visible
-    Given User visits SVT Play
-    Then Logo should be visible
+    Given user visits SVT Play
+    Then logo should be visible
 
   Scenario:Start link should exist
-    Given User visits SVT Play
-    Then Start link should exist and have the text "START"
+    Given user visits SVT Play
+    Then start link should exist and have the text "START"
 
   Scenario:Program link should exist
-    Given User visits SVT Play
-    Then Program link should exist and have the text "PROGRAM"
+    Given user visits SVT Play
+    Then program link should exist and have the text "PROGRAM"
 
   Scenario:Kanaler link should exist
-    Given User visits SVT Play
-    Then Kanaler link should exist and have the text "KANALER"
+    Given user visits SVT Play
+    Then kanaler link should exist and have the text "KANALER"
 
   Scenario:Tillgänglighet link should exist
-    Given User visits SVT Play
-    Then Tillganglighet link should exist
+    Given user visits SVT Play
+    Then tillganglighet link should exist
 
   Scenario:Clicking Tillgänglighet link should lead to correct page
-    Given User visits SVT Play
-    When User clicks tillganglighet link
-    Then Tillgänglighet page should display with header text "Så arbetar SVT med tillgänglighet"
+    Given user visits SVT Play
+    When user clicks tillganglighet link
+    Then tillgänglighet page should display with header text "Så arbetar SVT med tillgänglighet"
 
   Scenario:Verify 17 program categories is shown
-    Given User visits SVT Play
-    When User clicks on Program link
+    Given user visits SVT Play
+    When user clicks on Program link
     Then "17" categories are shown
 
   Scenario:Verify you can search on the site
-    Given User visits SVT Play
-    When User search for the text "ag"
-    Then search results header for text "ag" should display.
+    Given user visits SVT Play
+    When user search for the text "ag"
+    Then search results header for text "ag" should display
 
   Scenario:Cookie information link should display
-    Given User visits SVT Play
-    Then Cookie information link should display
+    Given user visits SVT Play
+    Then cookie information link should display
 
   Scenario:Cookie information link should lead to correct page
-    Given User visits SVT Play
-    When User clicks cookie information link
-    Then Cookie information page should display
+    Given user visits SVT Play
+    When user clicks cookie information link
+    Then cookie information page should display
 
   Scenario:TV tablå for SVT 1 should display
-    Given User visits SVT Play
-    When User clicks kanaler link
-    Then TVTablå for "SVT 1" should display.
+    Given user visits SVT Play
+    When user clicks kanaler link
+    Then TVTablå for "SVT 1" should display
 
   Scenario:TV tablå for SVT 2 should display
-    Given User visits SVT Play
-    When User clicks kanaler link
-    Then TVTablå for "SVT 2" should display.
+    Given user visits SVT Play
+    When user clicks kanaler link
+    Then TVTablå for "SVT 2" should display
 
   Scenario:TV tablå for SVT Barn should display
-    Given User visits SVT Play
-    When User clicks kanaler link
-    Then TVTablå for "SVT Barn" should display.
+    Given user visits SVT Play
+    When user clicks kanaler link
+    Then TVTablå for "SVT Barn" should display
 
   Scenario:TV tablå for Kunskapskanalen should display
-    Given User visits SVT Play
-    When User clicks kanaler link
-    Then TVTablå for "Kunskapskanalen" should display.
+    Given user visits SVT Play
+    When user clicks kanaler link
+    Then TVTablå for "Kunskapskanalen" should display
 
   Scenario:Nyhetsbrev link displays
-    Given User visits SVT Play
-    Then Nyhetsbrev link should display
+    Given user visits SVT Play
+    Then nyhetsbrev link should display
 
   Scenario:Nyhetsbrev link leads to correct page
-    Given User visits SVT Play
-    When User clicks Nyhetsbrev link
-    Then Nyhetsbrev page should display
+    Given user visits SVT Play
+    When user clicks Nyhetsbrev link
+    Then nyhetsbrev page should display
 
   Scenario:Search for Agenda shows first in the search result
-    Given User visits SVT Play
-    When User search for the text "Agenda"
-    Then search results header for text "Agenda" should display.
+    Given user visits SVT Play
+    When user search for the text "Agenda"
+    Then search results header for text "Agenda" should display
     Then program number "1" in the program list should be "Agenda"
 
   Scenario:Search for Pistvakt and verify number of episodes in season 2
-    Given User visits SVT Play
-    When User search for the text "Pistvakt"
-    Then search results header for text "Pistvakt" should display.
+    Given user visits SVT Play
+    When user search for the text "Pistvakt"
+    Then search results header for text "Pistvakt" should display
     When user clicks Pistvakt
     #Collapsing season1 below
     And clicks Pistvakt season one
@@ -96,6 +95,22 @@ Feature: Test page navigations and correct browser titles
     #Now only season 2 episodes are displaying
     Then "6" number of episodes are displaying
     Then episode "5" is called "5. Personalfestan"
+
+  Scenario:Dator link displays and navigates to correct page
+    Given user visits SVT Play
+    Then dator link should display
+    When user clicks on dator link
+    Then computer requirements page displays
+
+  Scenario:Search for Karen Pirie: Kalla fall shows first in the search result
+    Given user visits SVT Play
+    When user search for the text "Karen Pirie: Kalla fall"
+    Then search results header for text "Karen Pirie: Kalla fall" should display
+    When user clicks Karen Pirie: Kalla fall
+    Then "3" number of episodes are displaying
+    Then episode "1" is called "Avsnitt 1"
+
+
 
 
 
